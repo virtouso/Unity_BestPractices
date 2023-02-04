1ea808238823bf24df9ae4340631728d2dfdc526
virtouso <s_moeen_kamali@yahoo.com>
2023-02-04 06:17:43 +0330

    its mostly worst practice

--- UnityBestPractices/Assets/MVC/BaseModel.cs
+++ UnityBestPractices/Assets/MVC/BaseModel.cs
@@ -1,7 +1,13 @@
+using UnityEditor.Build.Content;
+using UnityEngine;
+
 namespace MVC.MVC
 {
     public abstract class BaseModel 
     {
-      
+        void xxx()
+        {
+            
+        }
     }
 }
